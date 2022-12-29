adefbbd2d5f2c67e8b486ca4a2cbc648053da1b1
ensar <ensar.makas@gmail.com>
2022-12-29 11:51:18 +0300

    Added new menus. They will be reconfigured

---
+++ src/components/Menus/index.jsx
@@ -0,0 +1,21 @@
+
+function Menus(){
+	// Responsive ayarlamaları yapılacak
+	return <>
+		<ul className="grid grid-flow-row-dense grid-cols-12 grid-rows-3">
+			<li className="mx-5 my-auto text-ground">ASD</li>
+
+			<li className="mx-5 my-auto text-ground">ASD</li>
+			
+			<li className="mx-5 my-auto text-ground">ASD</li>
+			
+			<li className="mx-5 my-auto text-ground">ASD</li>
+
+			<li className="mx-5 text-ground">ASD</li>
+			
+			
+		</ul>
+	</>
+}
+
+export {Menus}
