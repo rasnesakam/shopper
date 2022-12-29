59223bf49eb72f12ee48a76f2aa96ac4c658e9cf
ensar <ensar.makas@gmail.com>
2022-12-29 11:50:20 +0300

    Updated tailwind.config.js
    Added src directory to render. some colors and screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,26 @@ module.exports = {
   content: [
 	"./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+	"./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+		colors: {
+			primary: "#F26419",
+			secondary: "#216477",
+			ground:"#fefefe"
+		},
+		screens:{
+			'sm': '576px',
+			// => @media (min-width: 576px) { ... }
+
+			'md': '960px',
+			// => @media (min-width: 960px) { ... }
+
+			'lg': '1440px',
+			// => @media (min-width: 1440px) { ... }
+		}
+	},
   },
   plugins: [],
 }
