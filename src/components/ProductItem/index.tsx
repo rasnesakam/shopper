7ec1877bdfe7f8043ed44847c11c936e25250d45
ensar <ensar.makas@gmail.com>
2023-01-03 03:03:13 +0300

    Added product Item

---
+++ src/components/ProductItem/index.tsx
@@ -0,0 +1,25 @@
+import { AppProps } from "next/app";
+import Image from "next/image";
+import Link from "next/link";
+import Product from "../../types/Product";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart as faHeartSolid } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as faHeartRegular } from "@fortawesome/free-regular-svg-icons";
+
+const ProductItem = ({product}: {product: Product}) => {
+
+	return <>
+		<div className="relative border border-gray-200 rounded-lg shadow-md px-2 py-2">
+			<Link href={product.uri}>
+				<Image src={product.imageUrl} width={128} height={128} alt={product.name} />
+				<div>{product.name}</div>
+				<div>{product.price} <span>TL</span></div>
+			</Link>
+			<button className="absolute top-1 right-1 bg-white rounded-full flex items-center p-1">
+				<FontAwesomeIcon icon={faHeartRegular} className="text-red-500 text-sm" />
+			</button>
+		</div>
+	</>
+};
+
+export {ProductItem};
