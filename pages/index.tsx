223c64b2ff536c05ef8dabb199c45307252ba36a
ensar <ensar.makas@gmail.com>
2023-01-03 03:02:00 +0300

    redesigned index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import {NavBar} from "../src/components/NavBar"
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import {Slide} from '../src/components/Slide'
+import { ProductItem } from '../src/components/ProductItem'
+import Layout from "./layout"
 
 
 function HelloWorld(){
@@ -15,7 +17,20 @@ function HelloWorld(){
 
 export default function Home() {
   return (
-    <div>
+    <>
+		<Slide items={[
+			    "/images/batarya_dunyasi_design.png",
+			    "/images/logo_bd_180x180.png",
+		      ]}/>
+		<div className="m-5"></div>
+
+		<ProductItem product={{name:"item1",description:"item1",imageUrl:"/images/logo_bd_180x180.png",uri:"",price:10}}/>
+	</>
+  )
+}
+
+
+{/* <div>
 	      <Slide items={[
 			    "/images/batarya_dunyasi_design.png",
 			    "/images/logo_bd_180x180.png",
@@ -32,6 +47,4 @@ export default function Home() {
 			    <Image src="/images/batarya_dunyasi_design.png" alt="Vercel Logo" width={500} height={500} />
         </div>
 
-    </div>
-  )
-}
+    </div> */}
