92bf03af0bb9d3034cd6650fa9040edd86eebd74
ensar <ensar.makas@gmail.com>
2023-01-06 15:53:08 +0300

    Height and width properties converted to more dynamic view

--- src/components/ProductItem/index.tsx
+++ src/components/ProductItem/index.tsx
@@ -7,11 +7,13 @@ import { faHeart as faHeartSolid } from "@fortawesome/free-solid-svg-icons";
 import { faHeart as faHeartRegular } from "@fortawesome/free-regular-svg-icons";
 
 const ProductItem = ({product}: {product: Product}) => {
+	const height = 256;
+	const width = 256;
 
 	return <>
 		<div className="relative border border-gray-200 rounded-lg shadow-md px-2 py-2">
 			<Link href={product.uri}>
-				<Image src={product.imageUrl} width={128} height={128} alt={product.name} />
+				<Image src={product.imageUrl} width={width} height={height} alt={product.name} />
 				<div>{product.name}</div>
 				<div>{product.price} <span>TL</span></div>
 			</Link>
