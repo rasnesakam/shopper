4691b0fa474fe091c895fcc3a04cb691756758f5
ensar <ensar.makas@gmail.com>
2023-01-06 15:50:09 +0300

    Added timeout parameter to slide component. And added featured products component

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import styles from '../styles/Home.module.css'
 import {Slide} from '../src/components/Slide'
 import { ProductItem } from '../src/components/ProductItem'
 import Layout from "./layout"
+import { FeaturedProducts } from '../src/components/FeaturedProducts'
 
 
 function HelloWorld(){
@@ -18,33 +19,11 @@ function HelloWorld(){
 export default function Home() {
   return (
     <>
-		<Slide items={[
-			    "/images/batarya_dunyasi_design.png",
-			    "/images/logo_bd_180x180.png",
+		<Slide timeout={5000} items={[
+			    {imageUri: "/images/batarya_dunyasi_design.png",redirectTo: ""},
+			    {imageUri: "/images/logo_bd_180x180.png", redirectTo: ""},
 		      ]}/>
-		<div className="m-5"></div>
-
-		<ProductItem product={{name:"item1",description:"item1",imageUrl:"/images/logo_bd_180x180.png",uri:"",price:10}}/>
+		<FeaturedProducts />
 	</>
   )
 }
-
-
-{/* <div>
-	      <Slide items={[
-			    "/images/batarya_dunyasi_design.png",
-			    "/images/logo_bd_180x180.png",
-		      ]}/>
-
-        <h1 className={styles.title}>
-          Hoş Geldiniz!
-        </h1>
-
-        <p className={styles.description}>
-          Sayfamız şu anda yapım aşamasında. Size hizmet verebilmek için sabırsızlanıyoruz.
-        </p>
-        <div>
-			    <Image src="/images/batarya_dunyasi_design.png" alt="Vercel Logo" width={500} height={500} />
-        </div>
-
-    </div> */}
