81364caed8d5a8b843b42df7786f551d2a2c54ac
ensar <ensar.makas@gmail.com>
2023-01-07 00:12:48 +0300

    Account buttons moved to the component

--- src/components/AccountButtons/index.jsx
+++ src/components/AccountButtons/index.jsx
@@ -1,11 +1,26 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCartArrowDown } from "@fortawesome/free-solid-svg-icons"
+import {faHeart} from "@fortawesome/free-solid-svg-icons"
+import { faUser } from "@fortawesome/free-solid-svg-icons"
+
 function AccountButtons (){
-return<>
-<ul>
-    <li>Adanaya gidek</li>
-</ul>
+	return <>
+			<div className=" mr-5 flex flex-row justify-center">
 
+				<div className="my-auto px-1">
+					<FontAwesomeIcon icon={faUser} className="my-auto text-gray-500" />
+					<span className="my-auto pl-2">Hesabım</span>
+				</div>
+				<div className="my-auto px-1">
+					<FontAwesomeIcon icon={faHeart} className="my-auto text-gray-500 " />
+					<span className="my-auto pl-2">Favorilerim</span>
+				</div>
+				<div className="my-auto px-1">
+					<FontAwesomeIcon icon={faCartArrowDown} className="my-auto text-gray-500 " />
+					<span className="my-auto pl-2">Sepetim</span>
+				</div>
 
-
-</>
-
+			</div>
+		</>
 }
+export {AccountButtons}
