f6a99c294732adc9e3428af8133b26216ac86d30
ensar <ensar.makas@gmail.com>
2023-01-08 18:22:48 +0300

    Links added for each buttons

--- src/components/AccountButtons/index.jsx
+++ src/components/AccountButtons/index.jsx
@@ -2,22 +2,31 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCartArrowDown } from "@fortawesome/free-solid-svg-icons"
 import {faHeart} from "@fortawesome/free-solid-svg-icons"
 import { faUser } from "@fortawesome/free-solid-svg-icons"
+import Link from "next/link"
 
 function AccountButtons (){
+	const user = {};
 	return <>
 			<div className=" mr-5 flex flex-row justify-center">
 
 				<div className="my-auto px-1">
-					<FontAwesomeIcon icon={faUser} className="my-auto text-gray-500" />
-					<span className="my-auto pl-2">Hesabım</span>
+					<Link href="/hesap" className="group">
+						<FontAwesomeIcon icon={faUser} className="my-auto text-gray-500 group-hover:text-primary" />
+						<span className="my-auto pl-2 group-hover:text-primary">Hesabım</span>
+					</Link>
 				</div>
 				<div className="my-auto px-1">
-					<FontAwesomeIcon icon={faHeart} className="my-auto text-gray-500 " />
-					<span className="my-auto pl-2">Favorilerim</span>
+					<Link href="/fav" className="group">
+						<FontAwesomeIcon icon={faHeart} className="my-auto text-gray-500 group-hover:text-primary" />
+						<span className="my-auto pl-2  group-hover:text-primary">Favorilerim</span>
+					</Link>
+					
 				</div>
 				<div className="my-auto px-1">
-					<FontAwesomeIcon icon={faCartArrowDown} className="my-auto text-gray-500 " />
-					<span className="my-auto pl-2">Sepetim</span>
+					<Link href="/sepet" className="group">
+						<FontAwesomeIcon icon={faCartArrowDown} className="my-auto text-gray-500 group-hover:text-primary" />
+						<span className="my-auto pl-2 group-hover:text-primary">Sepetim</span>
+					</Link>
 				</div>
 
 			</div>
