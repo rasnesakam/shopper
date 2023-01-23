e8a1c20ce07afa6a6401848f7c81f2c3ba529d71
ensar <ensar.makas@gmail.com>
2023-01-23 11:31:13 +0300

    fix: Removed dotenv import from _app.tsx
    Added dotenv import to next.config.js. So one problem with
    undefined 'fs' is solved

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 const { statSync } = require('fs')
+require("dotenv").config();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'standalone'
+  output: 'standalone',
+  env: {
+	API_KEY:"",
+	SHOP_APP_URL: "http://localhost:5047"
+  }
 }
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,10 @@ import type { AppProps } from 'next/app'
 import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
 import { config } from "@fortawesome/fontawesome-svg-core";
 import Layout from './layout';
-import * as dotenv from "dotenv";
 
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
-	dotenv.config();
 	return <Layout>
 	  <Component {...pageProps} />
   	</Layout>
