36921936f3e2b2b57f9a0b602407e0d87763476b
ensar <ensar.makas@gmail.com>
2023-01-23 11:33:36 +0300

    fix: Fixed fetch data function. one field was unnecessary

--- src/functions/fetchData.ts
+++ src/functions/fetchData.ts
@@ -1,8 +1,9 @@
 
 async function fetcData<T>(url: string, queryOpts?: Array<{}>): Promise<Array<T>> {
 	let datas: T[]  = [];
-	await fetch(url).then(json => json.json()).then(data => {
-		data.datas.map((item: T) => {datas.push(item)});
+	await fetch(url).then(data => data.json()).then(data => {
+		console.log(data)
+		data.map((item: T) => {datas.push(item)});
 	});
 	return datas;
 }
