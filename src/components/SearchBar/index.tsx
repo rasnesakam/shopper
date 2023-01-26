7be0c55c40f91a85f6fa3b26da7d3f782288efbf
ensar <ensar.makas@gmail.com>
2023-01-26 13:38:05 +0300

    jsx converted to tsx

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -4,19 +4,20 @@ import {
     faAngleRight
   } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
+import { useRouter } from "next/router";
 
 const SearchBar = () => {
 
     const [value, setValue] = useState("");
+	const router = useRouter();
 
-    function submit(event){
+    function submit(event: any){
         event.preventDefault();
-        console.log(event);
-        alert(value);
+		router.push(`/arama/${encodeURIComponent(value)}`);
     }
 
     return <>
-    <div className="border-2 border-gray rounded-full p-1 ">
+    <div className="border-2 border-gray bg-ground rounded-full p-1 ">
 
         <form className="flex justify-between divide-x" onSubmit={submit}>
 
@@ -24,7 +25,7 @@ const SearchBar = () => {
                 <FontAwesomeIcon icon={faSearch} className="px-2" />
             </div>
             <div className="w-full">
-                <input type="text" placeholder="Ara" className="w-full px-1 focus:outline-none" onChange={e => setValue(e.target.value)} />
+                <input type="text" placeholder="Ara" className="w-full  px-1 focus:outline-none" onChange={e => setValue(e.target.value.toLowerCase())} />
             </div>
             <div className="">
                 <button className="hover:bg-primary hover:text-white hover:rounded-r-full">
