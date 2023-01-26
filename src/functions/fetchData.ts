de301eb68749cca5fc19aaf6a01f3f3b7ff2978d
ensar <ensar.makas@gmail.com>
2023-01-26 13:39:28 +0300

    wip: Added error handling for fetchData

--- src/functions/fetchData.ts
+++ src/functions/fetchData.ts
@@ -1,9 +1,17 @@
 
 async function fetcData<T>(url: string, queryOpts?: Array<{}>): Promise<Array<T>> {
 	let datas: T[]  = [];
-	await fetch(url).then(data => data.json()).then(data => {
-		console.log(data)
+	await fetch(url)
+	.then(response =>{
+		if (!response.ok)
+			throw Error(response.statusText)
+		return (response.json())
+	})
+	.then(data => {
 		data.map((item: T) => {datas.push(item)});
+	})
+	.catch(err => {
+		console.log(err);
 	});
 	return datas;
 }
