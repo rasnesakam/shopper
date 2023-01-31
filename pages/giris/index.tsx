d671b2799b48512bc2c554df341534beb859cd7b
ensar <ensar.makas@gmail.com>
2023-01-31 12:44:31 +0300

    Edited login page. Login will be via facebook(?) and google

--- pages/giris/index.tsx
+++ pages/giris/index.tsx
@@ -2,7 +2,9 @@ export default function giris(){
 
 	return <>
 	
-		NOT IMPLEMENTED YET
+		<div className="mx-auto my-32  p-10 border border-primary rounded-md shadow-xl">
+			Google ile giriş
+		</div>
 	
 	</>
 }
