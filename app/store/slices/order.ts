02ceed1a8aea70391b47312108991a71162dd114
ensar <ensar.makas@gmail.com>
2023-02-02 18:04:41 +0300

    Added new methods for order state

--- app/store/slices/order.ts
+++ app/store/slices/order.ts
@@ -1,4 +1,5 @@
 import { createSlice, Slice, SliceCaseReducers } from "@reduxjs/toolkit";
+import { HYDRATE } from "next-redux-wrapper";
 import Order from "../../../src/types/Order";
 import Product from "../../../src/types/Product";
 
@@ -10,6 +11,29 @@ export const OrderSlice: Slice<Order, SliceCaseReducers<Order>,"Order"> = create
 	reducers: {
 		setOrderData: (state, action) => {
 			state.products = action.payload;
+		},
+		addOrder: (state, action) => {
+			state.products.push(action.payload)
+		},
+		removeOrder: (state, action) => {
+			console.log(action.payload)
+			let index = state.products.indexOf(action.payload,0);
+			console.log(index);
+			if (index > -1)
+				state.products.splice(index,1);
+		}
+	},
+	extraReducers: {
+		[HYDRATE]: (state, action) => {return {
+				...state,
+				...action.payload.order
+			}
 		}
 	}
 })
+
+export const { setOrderData, addOrder, removeOrder } = OrderSlice.actions;
+
+
+
+export default OrderSlice.reducer;
