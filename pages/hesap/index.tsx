2a229e363c4556427bcd9341533cd37d0874cf37
ensar <ensar.makas@gmail.com>
2023-02-05 16:01:52 +0300

    Updated account page:
    - Added tabbed view for showing account settings
    - Added login redirection if no account logged in

--- pages/hesap/index.tsx
+++ pages/hesap/index.tsx
@@ -1,4 +1,47 @@
+import { Tab, Tabbed } from "../../src/components/TabbedPane";
+import { useDispatch, useSelector } from "react-redux";
+import { AppState } from "../../app/store";
+import authToken from "../../src/functions/authToken";
+
+function page(){
+	return <>
+		<Tabbed>
+			<Tab name="Adres Güncelle">
+				<div>
+					<form action="" className="flex flex-col gap-2">
+						<input type="text" name="name" placeholder="Adres adı" />
+						<input type="text" name="name" placeholder="Adres tipi" />
+						<input type="text" name="name" placeholder="Ülke" />
+						<input type="text" name="name" placeholder="Şehir" />
+						<textarea placeholder="Adres Satırı: 1"></textarea>
+						<textarea placeholder="Adres Satırı: 2"></textarea>
+						<input type="text" name="name" placeholder="Posta Kodu" />
+						<button type="submit">Kaydet</button>
+					</form>
+				</div>
+			</Tab>
+			<Tab name="Profil">
+				<form action="" className="flex flex-col gap-2">
+					<input type="text" name="name" placeholder="İsim" />
+					<input type="text" name="name" placeholder="Soyisim" />
+					<input type="phone" name="name" placeholder="+905xxxxxxxxx" />
+					<input type="email" name="name" placeholder="eposta@posta.com" />
+					<button type="submit">Kaydet</button>
+				</form>
+			</Tab>
+		</Tabbed>
+	</>
+}
+
+function logIn(){
+	return <>
+		You need to log in
+	</>
+}
 
 export default function hesap(){
-	return <>NOT IMPLEMENTED YET</>
+	const profile = useSelector((state: AppState) => state.profile);
+	//if (profile.authToken.length == 0 || !authToken(profile.authToken))
+	//	return logIn()
+	return page();
 }
