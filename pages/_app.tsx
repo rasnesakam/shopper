949bbeaa1d11aa75c6526f3dcf5a4b61846bea00
ensar <ensar.makas@gmail.com>
2023-02-09 15:00:28 +0300

    fix@_app: implementing persistent redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,19 @@ import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CS
 import { config } from "@fortawesome/fontawesome-svg-core";
 import Layout from './layout';
 import { wrapper } from "../app/store"
+import { Provider } from 'react-redux';
+import { PersistGate } from "redux-persist/integration/react"
+import { store } from '../src/redux/store';
+import { persistor } from '../src/redux/store';
 
 config.autoAddCss = false;
 
-function App({ Component, pageProps }: AppProps) {
-	return <Layout>
-	  <Component {...pageProps} />
-  	</Layout>
+export default function App({ Component, pageProps }: AppProps) {
+	return <Provider store={store}>
+		<PersistGate persistor={persistor}>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</PersistGate>
+	</Provider>
 }
-
-export default wrapper.withRedux(App);
