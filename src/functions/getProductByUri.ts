76424b8471aabdc122f0dab3b3e1e287202dc056
ensar <ensar.makas@gmail.com>
2023-02-09 14:58:27 +0300

    fix@getProductByUri: added param for uri

--- src/functions/getProductByUri.ts
+++ src/functions/getProductByUri.ts
@@ -2,8 +2,8 @@ import Product from "../types/Product";
 import config from "./config";
 import fetcData from "./fetchData";
 
-export default async function getProductByUri(): Promise<Product> {
-	let url = config.SHOP_APP_URL + "/Products"
+export default async function getProductByUri(uri: string): Promise<Product> {
+	let url = config.SHOP_APP_URL + "/Products/" + uri;
 	return await fetcData<Product>(url).then(list => list[0]);
 
 }
