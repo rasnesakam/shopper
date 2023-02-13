2615061bbc1fe415058921dac15ca299138fa628
ensar <ensar.makas@gmail.com>
2023-02-13 18:48:40 +0300

    added new env param and fixed syntax in .env

--- src/functions/config.ts
+++ src/functions/config.ts
@@ -2,5 +2,6 @@
 export default {
 	API_KEY: process.env.API_KEY ?? "",
 	SHOP_APP_URL: process.env.SHOP_APP_URL ?? "",
-	APP_NAME: process.env.APP_NAME ?? ""
+	APP_NAME: process.env.APP_NAME ?? "",
+	APP_NAME_NORMALIZED: process.env.APP_NAME_NORMALIZED ?? ""
 };
