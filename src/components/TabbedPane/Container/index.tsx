5dec28bd21ad44e301816b07a30ee18048634b19
ensar <ensar.makas@gmail.com>
2023-02-17 21:43:55 +0300

    update@component/TabbedPane: added external classname

--- src/components/TabbedPane/Container/index.tsx
+++ src/components/TabbedPane/Container/index.tsx
@@ -1,21 +1,26 @@
 import { useState } from "react"
 
-export default function Tabbed({children}: {children: React.ReactElement[]}){
-	const [currentEl, setCurrentEl] = useState(children[0]);
+export default function Tabbed({className, children}: {className?: string, children: React.ReactElement | React.ReactElement[]}){
+	const [currentEl, setCurrentEl] = useState(Array.isArray(children) ? children[0] : children);
 	const activeLink = "text-ground bg-primary"
 	return <>
-		<div className="mt-5 p-2 w-10/12 flex flex-row divide-x-2 border border-primary rounded-md">
+		<div className={`mt-5 p-2 flex flex-row divide-x-2 border border-primary rounded-md ${className}`}>
 			<div className="w-3/12">
 				<ul className="w-full divide-y-2">
 					{
-						children.map(
+						Array.isArray(children) ? children.map(
 							(item, index) => <li className="w-full my-1" key={index}>
 								<button className={`hover:text-ground hover:bg-primary w-full rounded-sm ${item == currentEl ? activeLink: ""}`}
 									onClick={() => setCurrentEl(item)}>
 									{item.props.name}
 								</button>
 							</li>
-						)
+						) : <li className="w-full my-1" key={0}>
+						<button className={`hover:text-ground hover:bg-primary w-full rounded-sm ${children == currentEl ? activeLink: ""}`}
+							onClick={() => setCurrentEl(children)}>
+							{children.props.name}
+						</button>
+					</li>
 					}
 				</ul>
 			</div>
