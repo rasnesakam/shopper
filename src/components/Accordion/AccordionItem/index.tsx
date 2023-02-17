b464a784fd96d81a619b10b3b8035f6a21c4bc13
ensar <ensar.makas@gmail.com>
2023-02-17 20:00:39 +0300

    update@component/Accordion: Added new icons for toggle button

--- src/components/Accordion/AccordionItem/index.tsx
+++ src/components/Accordion/AccordionItem/index.tsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown, faAngleUp } from "@fortawesome/free-solid-svg-icons";
 import React, { ReactElement, useState } from "react";
 import AccordionContext from "../AccordionContext";
 
@@ -13,7 +15,7 @@ export default function AccordionItem({id, title, children}: AccordionItemProps)
 		<AccordionContext.Consumer >
 		{
 			({visibleId, setVisibleId}) => <>		
-				<div className={`shadow-md py-2 text-lg font-semibold px-3 rounded-md`}>
+				<div className={`shadow-md py-2 text-lg font-semibold px-3 flex justify-between rounded-md`}>
 					{title}
 					
 					<button className="ml-5" onClick={
@@ -21,7 +23,7 @@ export default function AccordionItem({id, title, children}: AccordionItemProps)
 							console.log(id)
 							setVisibleId(id)
 						}
-					}>toggle</button>
+					}><FontAwesomeIcon icon={visibleId !== id ? faAngleDown: faAngleUp}/></button>
 				</div>
 				<div className={`px-3 py-1 border border-t-0 border-x-0 border-primary rounded-md ${visibleId !== id ? "hidden" : ""}`}>
 					{children}
