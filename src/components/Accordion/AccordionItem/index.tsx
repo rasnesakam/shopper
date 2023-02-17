01378689fd452a4c00e4a5ae87b61a75feac3e89
ensar <ensar.makas@gmail.com>
2023-02-17 20:37:48 +0300

    fix@component/Accordion: Click handler moved to the parent div of the title

--- src/components/Accordion/AccordionItem/index.tsx
+++ src/components/Accordion/AccordionItem/index.tsx
@@ -15,15 +15,16 @@ export default function AccordionItem({id, title, children}: AccordionItemProps)
 		<AccordionContext.Consumer >
 		{
 			({visibleId, setVisibleId}) => <>		
-				<div className={`shadow-md py-2 text-lg font-semibold px-3 flex justify-between rounded-md`}>
-					{title}
-					
-					<button className="ml-5" onClick={
+				<div className={`shadow-md py-2 text-lg font-semibold px-3 flex justify-between rounded-md cursor-pointer`}
+					onClick={
 						() => {
 							console.log(id)
 							setVisibleId(id)
 						}
-					}><FontAwesomeIcon icon={visibleId !== id ? faAngleDown: faAngleUp}/></button>
+					}>
+					{title}
+					
+					<i className="ml-5"><FontAwesomeIcon icon={visibleId !== id ? faAngleDown: faAngleUp}/></i>
 				</div>
 				<div className={`px-3 py-1 border border-t-0 border-x-0 border-primary rounded-md ${visibleId !== id ? "hidden" : ""}`}>
 					{children}
