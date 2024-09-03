6ab701968253d2aa94c59da5e823aecce5a98842
Ensar Makas <ensar.makas@gmail.com>
2024-09-03 21:01:17 +0300

    Added panel for handling product and order management

--- pages/panel/giris/index.tsx
+++ pages/panel/giris/index.tsx
@@ -3,7 +3,11 @@ export default function giris(){
 	return <>
 	
 		<div className="mx-auto my-32  p-10 border border-primary rounded-md shadow-xl">
-			Google ile giriş
+			<form action="/api/auth/login" method={"POST"} className="p-5 flex flex-col justify-center">
+				<input className="my-2 p-2 focus-visible:outline-0" name={"username"} type={"text"}  placeholder={"Username"}/>
+				<input className="my-2 p-2 focus-visible:outline-0" name={"password"} type={"password"}  placeholder={"Password"}/>
+				<button className="mt-5 p-2 rounded-md bg-primary hover:bg-primary/80 text-white" type="submit">Giriş</button>
+			</form>
 		</div>
 	
 	</>
---
+++ pages/panel/urunler/index.tsx
@@ -0,0 +1,7 @@
+export default function Urunler(){
+
+
+    return <>
+        {document.cookie.toString()}
+    </>
+}
