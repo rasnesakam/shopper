2ee2db83c9c2796f46e0ccf0fc6df136010a022b
Ensar Makas <ensar.makas@gmail.com>
2024-09-04 08:11:05 +0300

    Removed redux persist.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,20 +3,13 @@ import type { AppProps } from 'next/app'
 import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
 import { config } from "@fortawesome/fontawesome-svg-core";
 import Layout from './layout';
-import { wrapper } from "../app/store"
 import { Provider } from 'react-redux';
-import { PersistGate } from "redux-persist/integration/react"
-import { store } from '../src/redux/store';
-import { persistor } from '../src/redux/store';
+import { PersistGate } from "redux-persist/integration/react";
 
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Provider store={store}>
-		<PersistGate persistor={persistor}>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		</PersistGate>
-	</Provider>
+	return <Layout>
+		<Component {...pageProps} />
+	</Layout>
 }
