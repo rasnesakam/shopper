7223bdc7af37be8eed96de3c8125aa6ac95e445a
Ensar Makas <ensar.makas@gmail.com>
2024-09-04 08:12:01 +0300

    Updated product listing page

--- pages/panel/urunler/index.tsx
+++ pages/panel/urunler/index.tsx
@@ -1,7 +1,41 @@
-export default function Urunler(){
+import {useState} from "react";
+import Product from "@type/Product";
+import mockProducts from "@src/mock/products"
+export default function Products(){
 
+    const [products, setProducts] = useState<Product[]>(mockProducts);
+    const [search, setSearch] = useState("");
 
-    return <>
-        {document.cookie.toString()}
-    </>
+    return <div className="w-full flex flex-col items-center">
+        <input className="w-10/12 focus-visible:outline-primary p-2 m-2 border border-primary rounded-md" placeholder={"Ürün Ara"} value={search} onChange={(e) => setSearch(e.target.value)}/>
+        <table className="table-fixed w-full text-center">
+            <thead className="">
+            <tr>
+                <th>Görsel</th>
+                <th>Başlık</th>
+                <th>Kategori</th>
+                <th>Fiyat</th>
+            </tr>
+            </thead>
+            <tbody className="">
+                {products.map((product, index) => <tr key={`product-${index}`}>
+                    <td>
+                        <img className="w-16 h-16 mx-auto" src={product.productImages[0].fileUri}
+                             alt={product.productImages[0].altText}/>
+                    </td>
+                    <td>
+                        {product.name}
+                    </td>
+                    <td>
+                        {/* Kategori Gelecek */}
+                        {product.description}
+                    </td>
+                    <td>
+                        {/* Kategori Gelecek */}
+                        {product.price}₺
+                    </td>
+                </tr>)}
+            </tbody>
+        </table>
+    </div>
 }
