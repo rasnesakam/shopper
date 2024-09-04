db873a66b2bac67d3db920e22270e6434926bc9f
Ensar Makas <ensar.makas@gmail.com>
2024-09-04 08:11:18 +0300

    Removed old env parameters

--- next.config.js
+++ next.config.js
@@ -7,10 +7,6 @@ const nextConfig = {
   swcMinify: true,
   output: 'standalone',
   env: {
-	API_KEY:process.env.API_KEY,
-	SHOP_APP_URL: process.env.SHOP_APP_URL,
-	APP_NAME: process.env.APP_NAME,
-	APP_NAME_NORMALIZED: process.env.APP_NAME_NORMALIZED
   },
   images: {
 	remotePatterns: [
